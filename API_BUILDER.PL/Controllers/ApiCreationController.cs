6c3555c7a040e367d7d9b5fdea9cd0931ee7e492
SIBIARASU D <sibiarasu.cs20@bitsathy.ac.in>
2025-09-01 23:05:30 +0530

    Api Creation Logic

---
+++ API_BUILDER.PL/Controllers/ApiCreationController.cs
@@ -0,0 +1,33 @@
+﻿using API_BUILDER.IL;
+using API_BUILDER.MODEL;
+using API_BUILDER.PL.RESPONSE;
+using DATA_UTILITY.IL;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API_BUILDER.PL.Controllers
+{
+    public class ApiCreationController : ApiController
+    {
+        private readonly LoggerIL _logger;
+
+        public ApiCreationController(LoggerIL logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public ResponseModel GetApiDtls()
+        {
+            ResponseModel model = new ResponseModel();
+            return model;
+        }
+    }
+}
