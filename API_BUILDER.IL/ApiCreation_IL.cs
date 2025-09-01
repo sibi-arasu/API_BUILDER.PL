e78e5be4183d84f381196d648e8ae25cd0d26fca
SIBIARASU D <sibiarasu.cs20@bitsathy.ac.in>
2025-09-01 23:15:34 +0530

    IL Creation

---
+++ API_BUILDER.IL/ApiCreation_IL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_BUILDER.IL
+{
+    public class ApiCreation_IL
+    {
+        public DataSet GetApiDetails()
+        {
+            DataSet ds = new DataSet();
+            return ds;
+        }
+    }
+}
